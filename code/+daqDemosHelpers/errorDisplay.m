8a7648e22b0505d1cfabeb671bbd2062c5701339
Rob Campbell <git@raacampbell.com>
2017-05-04 15:20:26 +0200

    Improve error display function

--- code/+daqDemosHelpers/errorDisplay.m
+++ code/+daqDemosHelpers/errorDisplay.m
@@ -1,38 +1,43 @@
 function errorDisplay(ME)
-	% Neatly display a caught error message
-	% 
-	% function daqDemosHelpers.errorDisplay(ME)
-	%
-	% 
-	% Purpose
-	% We don't want to rethrow errors, but we do want to know what they
-	% are. This funcion displays a MATLAB error message structure to 
-	% the command line. 
-	%
-	% 
-	% Inputs
-	% ME - MATLAB error structure
-	%
-	% Outputs
-	% none
-	%
-	%
-	% Example usage
-	% In a function you might do:
-	%
-	%  try
-	%     %stuff here
-	%  catch ME
-	%     %stuff here
-	%     daqDemosHelpers.errorDisplay(ME)
-	%  end
-	%
-	%
-	%
-	%  Rob Campbell - Basel, 2017
-	%
-	% Also see: rethrow, dbstack, assert
+    % Neatly display a caught error message
+    % 
+    % function daqDemosHelpers.errorDisplay(ME)
+    %
+    % 
+    % Purpose
+    % We don't want to rethrow errors, but we do want to know what they
+    % are. This funcion displays a MATLAB error message structure to 
+    % the command line. 
+    %
+    % 
+    % Inputs
+    % ME - MATLAB error structure
+    %
+    % Outputs
+    % none
+    %
+    %
+    % Example usage
+    % In a function you might do:
+    %
+    %  try
+    %     %stuff here
+    %  catch ME
+    %     %stuff here
+    %     daqDemosHelpers.errorDisplay(ME)
+    %  end
+    %
+    %
+    %
+    %  Rob Campbell - Basel, 2017
+    %
+    % Also see: rethrow, dbstack, assert
 
 
-	%Print error to the command line
-  	fprintf('\nERRROR on line %d of %s: %s\n\n', ME.stack.line,  ME.stack.name, ME.message)
+    %Print error to the command line
+
+    fprintf('ERROR: %s\n',ME.message)
+    for ii=1:length(ME.stack)
+        fprintf(' on line %d of %s\n', ME.stack(ii).line,  ME.stack(ii).name)
+    end
+    fprintf('\n')
