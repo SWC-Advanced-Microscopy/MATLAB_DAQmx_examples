3f390f17dafe3941fd710b723ce575be6d6a33d4
Rob Campbell <git@raacampbell.com>
2017-07-19 16:23:59 +0200

    Optionally write data to a text file with vidrio.AI.hardwareContinuousVoltage

--- code/+vidrio/+AI/hardwareContinuousVoltage.m
+++ code/+vidrio/+AI/hardwareContinuousVoltage.m
@@ -1,4 +1,4 @@
-function hardwareContinuousVoltage
+function hardwareContinuousVoltage(fname)
     % A simple example showing hardware-timed continuous analog input using the Vidrio dabs.ni.daqmx wrapper
     %
     % function vidrio.AI.hardwareContinuousVoltage
@@ -7,7 +7,13 @@ function hardwareContinuousVoltage
     % Demonstrates how to do hardware-timed continuous analog input using Vidrio's dabs.ni.daqmx wrapper. 
     % This function continuously acquires data from multiple channels and plots the results to screen as
     % the data come in. Plotting is achieved in a very simple way with repeated call to plot in a while loop.
-    % The example uses the card's on-board clock but uses no triggers. 
+    % The example uses the card's on-board clock but uses no triggers. We optionally save data to a text file.
+    % Note that in "real" situations we'd likely want to use a binary format rather than ASCII. 
+    %
+    %
+    % Inputs
+    % fname - Relative path to file to which data will be written as a CSV file. 
+    %         If missing or empty, nothing is written to disk. 
     %
     %
     % Demonstrated steps:
@@ -15,11 +21,21 @@ function hardwareContinuousVoltage
     %    2. Create multiple Analog Input voltage channels.
     %    3. Set the sample rate, define the sample mode to be continuous.
     %    4. Call the Start function.
-    %    5. Read in the data continuously in a while loop
+    %    5. Read in the data continuously in a while loop, optionally saving to disk.
     %    6. Clear the task
     %    7. Display an error if any.
     %
     %
+    % Examples
+    % >> vidrio.AI.hardwareContinuousVoltage
+    % >> vidrio.AI.hardwareContinuousVoltage('myOutput.csv')
+    %
+    % The to read the data:
+    % >> T=textread('myOutput.csv','','delimiter',',');
+    % >> figure
+    % >> plot(T)
+    %
+    % 
     % Rob Campbell - Basel, 2017
     %
     % 
@@ -31,23 +47,33 @@ function hardwareContinuousVoltage
     % vidrio.AI.hardwareContinuousVoltageWithCallBack
     % PyDAQmx: https://pythonhosted.org/PyDAQmx/callback.html
 
-
     %Define a cleanup function
     tidyUp = onCleanup(@cleanUpFunction);
 
+    if nargin<1
+        fname=[];
+    end
+
     % Parameters for the acquisition (device and channels)
-    devName = 'Dev1';       % the name of the DAQ device as shown in MAX
+    devName = 'PXI1Slot3';       % the name of the DAQ device as shown in MAX
     taskName = 'hardAI';    % A string that will provide a label for the task
     physicalChannels = 0:3; % A scalar or an array with the channel numbers
-    minVoltage = -0.5;       % Channel input range minimum
-    maxVoltage = 0.5;        % Channel input range maximum
+    minVoltage = -10;       % Channel input range minimum
+    maxVoltage = 10;        % Channel input range maximum
 
 
     % Task configuration
     sampleClockSource = 'OnboardClock'; % The source terminal used for the sample Clock. 
                                         % For valid values see: zone.ni.com/reference/en-XX/help/370471AE-01/daqmxcfunc/daqmxcfgsampclktiming/
-    sampleRate = 10E3;                  % Sample Rate in Hz
+    sampleRate = 1E3;                  % Sample Rate in Hz
     numSamplesPerChannel = sampleRate*2 ; % The number of samples to be stored in the buffer per channel
+
+    if ~isempty(fname)
+        % So we write one channel per line in the optional csv file
+        formatStr = repmat('%0.5f,',1,length(physicalChannels));
+        formatStr(end)=[];
+        formatStr=[formatStr,'\n'];
+    end
 
 
     try
@@ -79,12 +105,24 @@ function hardwareContinuousVoltage
 
         fprintf('Recording data on %s. Hit ctrl-C to stop.\n', devName);
         clf 
+
+        if ~isempty(fname)
+            fid=fopen(fname,'w+');
+        else
+            fid=-1;            
+        end
+
         while 1
             % readAnalogData returns once 500 samples have been acquired or the 5 second timeout is reached
             % More details at:  "help dabs.ni.daqmx.Task.readAnalogData"
             data = hTask.readAnalogData(500,'scaled',5);
             plot(data)
             ylim([minVoltage,maxVoltage])
+
+            if fid>=0
+                fprintf(fid,formatStr, data');
+            end
+
             drawnow
         end
 
@@ -101,9 +139,14 @@ function hardwareContinuousVoltage
         if exist('hTask','var')
             fprintf('Cleaning up DAQ task\n');
             hTask.stop;    % Calls DAQmxStopTask
-            delete(hTask); % The destructor (dabs.ni.daqmx.Task.delete) calls DAQmxClearTask
+            delete(hTask); % The destructor (dabs.ni.daqmx.Task.delete) calls DAQmxClearTask           
         else
             fprintf('No task variable present for clean up\n')
+        end
+
+        if fid>=0
+            fprintf('Closing file\n')
+            fclose(fid);
         end
     end %close cleanUpFunction
 
